1836ac3b6316e0b05c2f17f788627a8c15986bb6
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-03 13:28:28 -0700

    Default Coder Girl GitHub Practice

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace Class1CoderGirlPractice
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine("What is your name?");
+            string name_input = Console.ReadLine();
+            string standardGreeting = ("Hello, " + name_input + ".");
+
+            if (name_input == "Amar")
+            {
+                Console.WriteLine(standardGreeting + " I like your shoes.");
+                Console.ReadLine(standardGreeting + " I like your shoes.");
+                
+
+            }
